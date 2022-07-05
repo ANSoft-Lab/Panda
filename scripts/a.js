c550ebb8f7a3c5652404cf14736329abd101c288
LIQIDATOR2007 <95560534+LIQIDATOR2007@users.noreply.github.com>
2022-07-05 14:17:58 +0600

    Delete scripts directory

--- scripts/a.js
+++
@@ -1,7 +0,0 @@
-function createActive(hbId, hbiId) {
-	let select = document.getElementById(hbiId);
-	let hb = document.getElementById(hbId);
-	select.addEventListener('click', function (){
-		hb.classList.toggle('active')
-	})
-}
